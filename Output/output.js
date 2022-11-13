6baee231b13cb84bb79dd5080189c4ecdb11b465
Emmanuel <godlytone@gmail.com>
2022-11-13 16:23:24 +0100

    value

---
+++ Output/output.js
@@ -0,0 +1,4 @@
+document.getElementById('lost').innerHTML="Javascript data type";
+document.getElementById('love').innerHTML="var length = 16";
+document.getElementById('lang').innerHTML="var lastName = Rick";
+document.getElementById('obey').innerHTML="var x = firstName:Rick,lastName:Monty";
