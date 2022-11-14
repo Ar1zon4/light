ceb1699ef65e3405ccc2bddcb8643862385fc5c7
Emmanuel <godlytone@gmail.com>
2022-11-14 11:35:09 +0100

    love

--- Assignment/assignment.js
+++ Assignment/assignment.js
@@ -1,1 +1,25 @@
-document.getElementById().innerHTML
+document.getElementById('love').innerHTML
+let var o = 10
+
+document.getElementById('vur').innerHTML
+
+document.getElementById('light').innerHTML
+
+
+document.getElementById('zero').innerHTML
+
+document.getElementById('foe').innerHTML
+
+document.getElementById('light').innerHTML
+
+document.getElementById('hero').innerHTML
+
+document.getElementById('win').innerHTML
+
+document.getElementById('vur').innerHTML
+
+document.getElementById('vur').innerHTML
+
+document.getElementById('vur').innerHTML
+
+
